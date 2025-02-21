4322717154328ae40094674a1dd760ab58604062
yashilbusa <yashilpatel1914@gmail.com>
2025-02-21 10:57:12 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,25 @@
+import express from 'express'
+import mongoose from 'mongoose';
+
+const app = express()
+const port = 4004
+
+try {
+    await mongoose.connect(process.env.url);
+    console.log('Connected to MongoDB');
+    
+    app.listen(port, () => {
+      console.log(`Server Running on http://localhost:${port}`);
+    });
+  } catch (error) {
+    console.error('Error connecting to MongoDB:', error.message);
+}
+
+const StudentSchema = new mongoose.Schema({
+  studentName:String,
+  subject:String,
+  grade:Number
+})
+const StudentGrade = mongoose.model('StudentGrade',StudentSchema)
+
+app.use(express.json())
