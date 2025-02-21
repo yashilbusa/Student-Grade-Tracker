6567acb0509dc1e65de5ac87ae6d4d27586a376c
yashilbusa <yashilpatel1914@gmail.com>
2025-02-21 14:38:57 +0530

    commit

--- index.js
+++ index.js
@@ -26,6 +26,7 @@ const StudentSchema = new mongoose.Schema({
 })
 const Student = mongoose.model('Student',StudentSchema)
 
+// app.use(express.static("public"))
 app.use(express.json())
 app.use(express.urlencoded({ extended: true })); 
 app.set('view engine','ejs')
